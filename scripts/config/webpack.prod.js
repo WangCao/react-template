375fb40d9e7fb0ef38c299f7f59781deca911f50
wangcao <wangcao@xiaomi.com>
2020-08-19 21:01:03 +0800

    添加bundle分析

--- scripts/config/webpack.prod.js
+++ scripts/config/webpack.prod.js
@@ -4,6 +4,7 @@ const PurgeCSSPlugin = require('purgecss-webpack-plugin');
 const glob = require('glob');
 const { resolve } = require('path');
 const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const common = require('./webpack.common.js');
 const { PROJECT_PATH } = require('../constant');
 
@@ -21,6 +22,11 @@ module.exports = merge(common, {
     new webpack.BannerPlugin({
       raw: true,
       banner: '/** @preserve Powered by wangcao */',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server', // 开一个本地服务查看报告
+      analyzerHost: '127.0.0.1', // host 设置
+      analyzerPort: 8888, // 端口号设置
     }),
   ],
 });
